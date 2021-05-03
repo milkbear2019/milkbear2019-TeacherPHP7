2aea99202b200988b9bfa7443f6507d440910cb5
james <milkbear2019@gmail.com>
2021-05-03 14:40:40 +0800

    placerholder/min/pattern設定

--- ch05/自行輸入計算階乘.php
+++ ch05/自行輸入計算階乘.php
@@ -1,32 +1,31 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
-	<link rel="stylesheet" href="">
-</head>
-<body>
-	<?php
-	function showResult($Num){
-		if ($Num >= 0){
-			if ($Num == 0){
-				return 1;
-			}else{
-				$resultNum = 1;
-				for($n=$Num;$n>0;$n--){ //n = n -1
-					$resultNum *= $n; //$resultNum = $resultNum * n
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title></title>
+		<link rel="stylesheet" href="">
+	</head>
+	<body>
+		<?php
+		function showResult($Num){
+			if ($Num >= 0){
+				if ($Num == 0){
+					return 1;
+				}else{
+					$resultNum = 1;
+					for($n=$Num;$n>0;$n--){ //n = n -1
+						$resultNum *= $n; //$resultNum = $resultNum * n
+					}
+					return $resultNum;
 				}
-				return $resultNum;
 			}
 		}
-	}
-	if(isset($_GET['num'])) echo $_GET['num']." 的階乘為：" . showResult($_GET['num']);
-?>
-<form action="" method="get" accept-charset="utf-8">
-請輸入所數字:
-<input type="text" name="num">		
-<input type="submit" value="送出">
-</form>
-</body>
+		if(isset($_GET['num'])) echo $_GET['num']." 的階乘為：" . showResult($_GET['num']);
+		?>
+		<form action="" method="get" accept-charset="utf-8">
+			<input type="text" name="num" placeholder="輸入所要計算階乘數字" min="0" pattern="[0-9]" title='請輸入正整數' required>
+			<input type="submit" value="送出">
+		</form>
+	</body>
 </html>
