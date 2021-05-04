0062cf79c906f3dbecabf34942290bd7c18dc70f
james <milkbear2019@gmail.com>
2021-05-04 16:25:30 +0800

    陣列排序

--- ch06/php_array12.php
+++ ch06/php_array12.php
@@ -1,12 +1,35 @@
 <?php
 $fruits = array("d" => "lemon", "a" => "orange", "b" => "banana", "c" => "apple");
 sort($fruits);
+echo "sort<br>";
 foreach ($fruits as $key => $val) {
     echo "$key = $val <br />";
 }
 echo "<hr />";
 $fruits = array("d" => "lemon", "a" => "orange", "b" => "banana", "c" => "apple");
 asort($fruits);
+echo "asort<br>";
+foreach ($fruits as $key => $val) {
+    echo "$key = $val <br />";
+}
+echo "<hr />";
+$fruits = array("d" => "lemon", "a" => "orange", "b" => "banana", "c" => "apple");
+arsort($fruits);
+echo "arsort<br>";
+foreach ($fruits as $key => $val) {
+    echo "$key = $val <br />";
+}
+echo "<hr />";
+$fruits = array("d" => "lemon", "a" => "orange", "b" => "banana", "c" => "apple");
+ksort($fruits);
+echo "ksort<br>";
+foreach ($fruits as $key => $val) {
+    echo "$key = $val <br />";
+}
+echo "<hr />";
+$fruits = array("d" => "lemon", "a" => "orange", "b" => "banana", "c" => "apple");
+krsort($fruits);
+echo "krsort<br>";
 foreach ($fruits as $key => $val) {
     echo "$key = $val <br />";
 }
