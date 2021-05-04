01fa4ceadfccc6e7898987b4f245a706686a35a5
james <milkbear2019@gmail.com>
2021-05-04 14:37:36 +0800

    修改php_array8

--- ch06/php_array8.php
+++ ch06/php_array8.php
@@ -1,8 +1,8 @@
 <?php
 $season = array('春', '夏', '秋', '冬'); 
 
-echo "每年的四季分別為：";
-foreach ($season as $value){
-	echo $value;
+echo "每年的四季分別為：<br>";
+foreach ($season as $key => $value){
+	echo '$season陣列的第 '.($key+1)."元素內容是: ".$value."<br>";
 }
 ?>
