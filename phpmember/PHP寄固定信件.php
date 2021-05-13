0ef1b4e73e56ec7dd2f167fd64e596e1e7cc8466
james <milkbear2019@gmail.com>
2021-05-13 10:41:36 +0800

    寄因定信標題修改

--- phpmember/PHP寄固定信件.php
+++ phpmember/PHP寄固定信件.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>寄固定信件</title>
+    <title>寄固定信件範例</title>
 </head>
     <body>
         <?php
